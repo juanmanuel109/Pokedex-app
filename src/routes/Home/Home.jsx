7e92ebb8ac42dd29cfd643d0f193046cc804724a
jdcarvajalc <jdcc1308@gmail.com>
2024-05-16 15:46:58 -0500

    refactor Home component and CSS and update layout and styling

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -2,39 +2,23 @@ import React from "react";
 import styles from "./Home.module.css";
 
 const Home = () => {
-    const pokemons = [
-        { id: 1, name: "Bulbasaur", number: "001", image: "bulbasaur.png" },
-        { id: 4, name: "Charmander", number: "004", image: "charmander.png" },
-    ];
-
     return (
-        <div className={styles.outerContainer}>
-            <div className={styles.browserHeader}>
-                <div className={styles.circle}></div>
-                <div className={styles.circle}></div>
-                <div className={styles.circle}></div>
-                <h1>Pokedex</h1>
-                <button className={styles.addButton}>+ Add New Pokemon</button>
-            </div>
-            <div className={styles.header}></div>
-            <div className={styles.container}>
-                <div className={styles.pokemonList}>
-                    {pokemons.map((pokemon) => (
-                        <div key={pokemon.id} className={styles.pokemonCard}>
-                            <img src={pokemon.image} alt={pokemon.name} />
-                            <div className={styles.pokemonInfo}>
-                                <span className={styles.pokemonNumber}>
-                                    {pokemon.number}
-                                </span>
-                                <span className={styles.pokemonName}>
-                                    {pokemon.name}
-                                </span>
-                            </div>
-                        </div>
-                    ))}
+        <div className={styles.homeContainer}>
+            <div className={styles.header}>
+                <div className={styles.staticIcons}>
+                    <div className={styles.staticBigCircle}></div>
+                    <div className={styles.staticSmallCircles}>
+                        <div className={styles.circle}></div>
+                        <div className={styles.circle}></div>
+                        <div className={styles.circle}></div>
+                    </div>
+                </div>
+                <div className={styles.navbar}>
+                    <a href="">Inicio</a>
+                    <a href="">Salir</a>
                 </div>
             </div>
-            <footer className={styles.footer}>Copyright © 2019</footer>
+            <div className={styles.container}></div>
         </div>
     );
 };
