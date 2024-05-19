ac39c882c42abc8680fa34937d8500f26e69206e
jdcarvajalc <jdcc1308@gmail.com>
2024-05-19 10:09:03 -0500

    add comments and new queries for create, update, and delete operations to queries.sql file

--- db/queries.sql
+++ db/queries.sql
@@ -1,6 +1,9 @@
 /* 
- Operaciones de lectura
+ Estas consultas tienen código php incrustado, es necesario usarlas dentro de un entorno adecuado
  */
+-- ******************************************************************************************************
+-- Operaciones de lectura
+-- ******************************************************************************************************
 -- Leer atributos clave de todos los pokemones
 SELECT pok_id,
     pok_name,
@@ -26,6 +29,8 @@ FROM pokemon
 WHERE pokemon.pok_id = $pok_id;
 --
 -- Leer los tipos del Pokémon seleccionado
+-- Es necesario resaltar el uso de GROUP_CONCAT
+-- Esta función permite concatenar varios registros en uno solo delimitados con una ','
 SELECT pok_height,
     pok_weight,
     pok_base_experience,
@@ -34,3 +39,34 @@ FROM pokemon
     JOIN pokemon_types ON pokemon_types.pok_id = pokemon.pok_id
     JOIN TYPES ON TYPES.type_id = pokemon_types.type_id
 WHERE pokemon.pok_id = $pok_id;
+-- ******************************************************************************************************
+-- Operaciones de escritura
+-- ******************************************************************************************************
+-- Crear un nuevo Pokémon
+INSERT INTO pokemon (
+        pok_name,
+        pok_height,
+        pok_weight,
+        pok_base_experience
+    )
+VALUES (
+        '$pok_name',
+        '$pok_height',
+        '$pok_weight',
+        '$pok_base_experience';
+-- ******************************************************************************************************
+-- Operaciones de actualización
+-- ******************************************************************************************************
+-- Actualizar datos del Pokémon seleccionado
+UPDATE pokemon
+SET pok_name = '$pok_name',
+    pok_height = '$pok_height',
+    pok_weight = '$pok_weight',
+    pok_base_experience = '$pok_base_experience'
+WHERE pok_id = $pok_id;
+-- ******************************************************************************************************
+-- Operaciones de eliminación
+-- ******************************************************************************************************
+-- Eliminar el Pokémon seleccionado
+DELETE FROM pokemon
+WHERE pok_id = $pok_id;
